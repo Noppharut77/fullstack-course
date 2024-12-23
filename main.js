12c6c69b71b5428b4cfa2dec29bc99c12d1f6b2a
DESKTOP-JNJBSKQ\User <nutipalakawong.88@gmail.com>
2024-12-23 21:19:56 +0700

    add index.html and style.css for init project

---
+++ main.js
@@ -0,0 +1,52 @@
+/* + ได้เฉพาะ string
+== เท่ากับ
+!==ไม่เท่ากับ
+> น้อยกว่า
+>=น้อยกว่าหรือเท่ากับ
+< มากกว่า
+<= มากกว่าหรือเท่ากับ
+
+&& และ
+|| หรือ
+! not 
+
+while
+for = shorthand ของ while
+
+function = ใส่สิ่งที่เหมือนกัน รวมเข้าไปในคำสั่งใหม่
+*/
+
+// object function
+
+let students = [
+{
+    name: 'ter',
+    score: 30,
+    grade: 'f'
+
+},
+{
+    name: 'tangkwa',
+    score: 60,
+    grade: 'B'
+
+},
+{
+    name: 'champ',
+    score: 90,
+    grade: 'A+++'
+
+}
+]
+
+let student = students.find((s) => {
+ if (s.name == 'tangkwa') {
+    return true
+ }
+})
+
+let hightscore_student = students.filter((s)=> {if (s.score >= 60) {return true}
+})
+
+console.log('student:', student)
+console.log('hightscore_student', hightscore_student)
