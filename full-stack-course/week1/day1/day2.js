e6e4dce1a4282e5054d622369642da1db3b2ee06
DESKTOP-JNJBSKQ\User <nutipalakawong.88@gmail.com>
2025-11-03 21:35:05 +0700

    Day 2: Operators, precedence, type coercion

---
+++ full-stack-course/week1/day1/day2.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 5;
+
+console.log("ผลบวก:", a + b);
+console.log("ผลลบ:", a - b);
+console.log("ผลคูณ:", a * b);
+console.log("ผลหาร:", a / b);
+console.log("เศษจาการหาร:", a % b);
+console.log("ผลยกกำลัง:", a ** b);
